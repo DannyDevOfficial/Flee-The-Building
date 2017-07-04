8a5f6e9d4a7e3bddc08e382c21194ade7fd42119
Daniel Grieco <danny26.business@gmail.com>
2017-07-04 16:43:57 +0200

    Added a component (C++ class) that takes care of opening doors.

---
+++ Source/FleeTheBuilding/DoorOpener.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DoorOpener.h"
+
+#include <Engine/World.h>
+#include <GameFramework/PlayerController.h>
+
+
+// Sets default values for this component's properties
+UDoorOpener::UDoorOpener()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDoorOpener::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// If there isn't a pressure plate...
+	if (pressurePlate->IsActorInitialized())
+		// Log error to output log
+		UE_LOG(LogTemp, Error, TEXT("No pressure plate in the level!"));
+
+	// Get the actor that opens
+	actorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	// If the actor wasn't initialized
+	if (actorThatOpens->IsActorInitialized())
+		// Log error to output log
+		UE_LOG(LogTemp, Error, TEXT("No actor in the level!"));
+	
+	// Get time since last open
+	timeSinceLastOpen = GetWorld()->GetTimeSeconds();
+}
+
+
+// Called every frame
+void UDoorOpener::TickComponent(float DeltaTime,
+								ELevelTick TickType,
+								FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Handle door open and close
+	RunDoorMechanism();
+}
+
+void UDoorOpener::Open() {
+	// Open the door by setting its rotation to open angle
+	GetOwner()->SetActorRotation(FRotator(0.0f, openAngle, 0.0f));
+	// Set time since last open to now
+	timeSinceLastOpen = GetWorld()->GetTimeSeconds();
+}
+
+void UDoorOpener::Close() {
+	// Close the door by setting its rotation back to 0
+	GetOwner()->SetActorRotation(FRotator(0.0f, 0.0f, 0.0f));
+}
+
+void UDoorOpener::RunDoorMechanism() {
+	// If the actor is on pressure plate
+	if (pressurePlate->IsOverlappingActor(actorThatOpens))
+		// Open it
+		Open();
+
+	// Close the door when more than the given amount of secs have passed since
+	// the door was last open
+	if (GetWorld()->GetTimeSeconds() - timeSinceLastOpen > timeBeforeClosing)
+		Close();
+}
+
---
+++ Source/FleeTheBuilding/DoorOpener.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <Engine/TriggerVolume.h>
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DoorOpener.generated.h"
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+/// <summary>
+/// A class that commands doors' opening
+/// </summary>
+/// <seealso cref="UActorComponent" />
+class FLEETHEBUILDING_API UDoorOpener: public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDoorOpener();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime,
+							   ELevelTick TickType,
+							   FActorComponentTickFunction* ThisTickFunction) override;
+
+private:	
+	/// <summary>
+	/// Opens this instance of the door.
+	/// </summary>
+	void Open();
+	
+	/// <summary>
+	/// Closes this instance of the door.
+	/// </summary>
+	void Close();
+	
+	/// <summary>
+	/// Takes care of opening and closing the door.
+	/// </summary>
+	void RunDoorMechanism();
+
+private:
+	// Angle at which a door will open
+	UPROPERTY(EditAnywhere)
+		float openAngle = 90.0f;
+
+	// Time to wait before door closes
+	UPROPERTY(EditAnywhere)
+		float timeBeforeClosing = 2.0f;
+
+	// Pressure Plate that will allow actor to open the door
+	UPROPERTY(EditAnywhere)
+		ATriggerVolume* pressurePlate = nullptr;
+
+	// Actor that will trigger the opening
+	AActor* actorThatOpens = nullptr;
+
+	// Time passed since the door was last open
+	float timeSinceLastOpen = 0.0f;
+};
